63e09603ec3ab811326b33c80e45ef005429e972
season <zhujilong1987@163.com>
2022-08-12 10:19:51 +0800

    避免不停的监听图片资源

--- lib/base/interface.dart
+++ lib/base/interface.dart
@@ -14,3 +14,13 @@ abstract class IWebLoadInfo {
   String? title;
   String? link;
 }
+
+class Box<T extends Object> {
+  T value;
+
+  Box(this.value);
+}
+
+extension Extension<T extends Object> on T {
+  Box<T> get box => Box<T>(this);
+}
