ec2556a0ab3b61065650625621b8b53919b25072
season <zhujilong1987@163.com>
2023-08-10 10:00:49 +0800

    no message

--- lib/pages/common/status_view.dart
+++ lib/pages/common/status_view.dart
@@ -3,11 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'package:getx_study/base/base_controller.dart';
+import 'package:getx_study/enum/response_status.dart';
 import 'loading_view.dart';
 import 'error_view.dart';
 import 'empty_view.dart';
 
-typedef WidgetCallback<T extends BaseController>  = Widget Function(T);
+typedef WidgetCallback<T extends BaseController> = Widget Function(T);
 
 /// 响应View,这个view和网络请求的回调紧密联系,是我经过几次思考后得出的方案
 class StatusView<T extends BaseController> extends StatelessWidget {
@@ -15,8 +16,7 @@ class StatusView<T extends BaseController> extends StatelessWidget {
 
   final String? tag;
 
-  const StatusView(
-      {Key? key, required this.contentBuilder, this.tag})
+  const StatusView({Key? key, required this.contentBuilder, this.tag})
       : super(key: key);
 
   @override
@@ -28,12 +28,34 @@ class StatusView<T extends BaseController> extends StatelessWidget {
           index: controller.status.value,
           children: [
             const LoadingView(),
-            ErrorView(retryAction: controller.retry,),
+            ErrorView(
+              retryAction: controller.retry,
+            ),
             contentBuilder(controller),
-            EmptyView(emptyTap: controller.emptyTap,)
+            EmptyView(
+              emptyTap: controller.emptyTap,
+            )
           ],
         );
       }),
     );
+  }
+
+  /// 这种方式好像在构建首页的时候无法触发首页刷新操作,导致无法做首页的网络请求,正常的页面无法构建
+  Widget responseStatusWidgetWithSwitch(T controller) {
+    switch (controller.status) {
+      case ResponseStatus.loading:
+        return const LoadingView();
+      case ResponseStatus.fail:
+        return ErrorView(
+          retryAction: controller.retry,
+        );
+      case ResponseStatus.successHasContent:
+        return contentBuilder(controller);
+      case ResponseStatus.successNoData:
+        return EmptyView(
+          emptyTap: controller.emptyTap,
+        );
+    }
   }
 }
