f59d16eb7db7401768577875e17d868972f7238e
season <zhujilong1987@163.com>
2023-08-31 09:36:05 +0800

    GetMiddleware的使用例子尝试

--- lib/routes/login_middleware.dart
+++ lib/routes/login_middleware.dart
@@ -6,6 +6,7 @@ import 'package:getx_study/account_manager/account_manager.dart';
 import 'package:getx_study/routes/routes.dart';
 
 class LoginMiddleware extends GetMiddleware {
+  /// 当需要执行路由的重定向时，就可以调用此函数，比如使用它实现强制登录逻辑，即没有登录时跳转登录逻辑。
   @override
   RouteSettings? redirect(String? route) {
     return AccountManager().isLogin ? null : const RouteSettings(name: Routes.login);
