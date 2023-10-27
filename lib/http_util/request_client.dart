6dec126c60d8fddc35c7c03a96b87fd4417999dd
season <zhujilong1987@163.com>
2023-10-27 15:13:58 +0800

    使用Retrofit脚本编写网络接口

--- lib/http_util/request_client.dart
+++ lib/http_util/request_client.dart
@@ -9,9 +9,16 @@ import 'package:getx_study/entity/base_entity.dart';
 import 'package:getx_study/entity/banner_entity.dart';
 import 'package:getx_study/entity/article_info_entity.dart';
 import 'package:getx_study/entity/page_entity.dart';
+import 'package:getx_study/entity/hot_key_entity.dart';
+import 'package:getx_study/entity/tab_entity.dart';
+import 'package:getx_study/entity/account_info_entity.dart';
+import 'package:getx_study/entity/coin_rank_entity.dart';
+import 'package:getx_study/entity/my_coin_history_entity.dart';
 import 'package:retrofit/retrofit.dart';
 
 part 'request_client.g.dart';
+
+/// 参看 说明.md中的执行脚本
 
 const timeout = Duration(seconds: 60);
 
@@ -38,13 +45,75 @@ abstract class RequestClient {
   // baseUrl: 请求域名，优先级高于注解
   factory RequestClient(Dio dio, {String baseUrl}) = _RequestClient;
 
-  @GET(Api.getBanner)
+  @GET("banner/json")
   Future<BaseEntity<List<BannerEntity>>> getBanner();
 
-  @GET(Api.getTopArticleList)
-  Future<BaseEntity<List<ArticleInfoDatas>>> getTopArticleList();
-
-  @GET("${Api.getArticleList}{page}/json")
+  @GET("article/list/{page}/json")
   Future<BaseEntity<PageEntity<List<ArticleInfoDatas>>>> getArticleList(
       @Path() int page);
+
+  @GET("article/top/json")
+  Future<BaseEntity<List<ArticleInfoDatas>>> getTopArticleList();
+
+  @GET("getSearchHotKey")
+  Future<BaseEntity<List<HotKeyEntity>>> getSearchHotKey();
+
+  @POST("article/query/{page}/json")
+  Future<BaseEntity<PageEntity<List<ArticleInfoDatas>>>> searchKeyword(
+      @Path() int page, @Query("k") String keyword);
+
+  @GET("project/tree/json")
+  Future<BaseEntity<List<TabEntity>>> getProjectTab();
+
+  @GET("project/list/{page}/json")
+  Future<BaseEntity<PageEntity<List<ArticleInfoDatas>>>>
+      getCurrentProjectTabList(@Path() int page, @Query("cid") String id);
+
+  @GET("wxarticle/chapters/json")
+  Future<BaseEntity<List<TabEntity>>> getWXArticleTab();
+
+  @GET("wxarticle/list/{id}/{page}/json")
+  Future<BaseEntity<PageEntity<List<ArticleInfoDatas>>>>
+      getCurrentWXArticleTabList(@Path() String id, @Path() int page);
+
+  @POST("user/login")
+  Future<BaseEntity<AccountInfoEntity>> login(
+      @Query("username") String username, @Query("password") String password);
+
+  @POST("user/register")
+  Future<BaseEntity<AccountInfoEntity>> register(
+      @Query("username") String username,
+      @Query("password") String password,
+      @Query("repassword") String repassword);
+
+  @GET("user/logout/json")
+  Future<BaseEntity<Object?>> logout();
+
+  @POST("lg/collect/{id}/json")
+  Future<BaseEntity<Object?>> collectAction(@Path() int originId);
+
+  @POST("lg/uncollect_originId/{id}/json")
+  Future<BaseEntity<Object?>> unCollectAction(@Path() int originId);
+
+  @GET("lg/collect/list/{page}/json")
+  Future<BaseEntity<PageEntity<List<ArticleInfoDatas>>>> getCollectArticleList(
+      @Path() int page);
+
+  @GET("coin/rank/{page}/json")
+  Future<BaseEntity<PageEntity<List<CoinRankDatas>>>> getCoinRankList(
+      @Path() int page);
+
+  @GET("lg/coin/list/{page}/json")
+  Future<BaseEntity<PageEntity<List<MyCoinHistoryDatas>>>> getMyCoinList(
+      @Path() int page);
+
+  @GET("lg/coin/userinfo/json")
+  Future<BaseEntity<CoinRankDatas>> getUserCoinInfo();
+
+  @GET("tree/json")
+  Future<BaseEntity<List<TabEntity>>> getTreeTab();
+
+  @GET("article/list/{id}/{page}/json")
+  Future<BaseEntity<PageEntity<List<ArticleInfoDatas>>>> getCurrentTreeTabList(
+      @Path() String id, @Path() int page);
 }
