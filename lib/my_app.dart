1979f8ae70da333393a21f9384b8d697256f964f
season <zhujilong1987@163.com>
2024-01-24 15:23:26 +0800

    window过期方法优化

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/scheduler.dart';
+
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 import 'package:get/get.dart';
 import 'package:getx_study/base/getx_router_observer.dart';
 import 'package:getx_study/extension/theme_data_extension.dart';
 import 'package:getx_study/logger/logger.dart';
-import 'routes/routes.dart';
+import 'package:getx_study/routes/routes.dart';
 
 class MyApp extends StatelessWidget {
   final bool isFirst;
@@ -37,7 +37,9 @@ class MyApp extends StatelessWidget {
 
   /// App运行过程中,如果在iOS的设置中更改了亮度模式,还是无法实时进行更改,只能下次运行的时候才能体现变化,体验不好
   ThemeData _getMaterialCurrentTheme() {
-    return SchedulerBinding.instance.window.platformBrightness.themeData;
+    return WidgetsBinding.instance.platformDispatcher.platformBrightness.themeData;
+    //return View.of(context).platformDispatcher.platformBrightness.themeData;
+    //return SchedulerBinding.instance.window.platformBrightness.themeData;
   }
 
   CupertinoThemeData _getCupertinoCurrentTheme() {
