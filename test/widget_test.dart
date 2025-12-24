c00fcbc4ddd245e65a6ddec1a698df37165f31ec
season <zhujilong1987@163.com>
2025-12-24 14:58:17 +0800

    简单弄了一个test，尝试在Github上面的Action可以通过

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:getx_study/my_app.dart';
+import 'package:getx_study/example_app/get_x_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp(isFirst: true,));
+    await tester.pumpWidget(GetXApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
